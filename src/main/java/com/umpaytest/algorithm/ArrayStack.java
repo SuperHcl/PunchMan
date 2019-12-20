5d68cbf301d53d97015987611c1c59da0f25b6ea
hcl <157223522@qq.com>
2019-12-20 14:25:42 +0800

    数组实现的顺序栈

--- src/main/java/com/umpaytest/algorithm/ArrayStack.java
+++ src/main/java/com/umpaytest/algorithm/ArrayStack.java
@@ -8,11 +8,11 @@ package com.umpaytest.algorithm;
 public class ArrayStack {
     private int size;       //栈的大小
     private int count;      //栈中元素的个数
-    private int[] data;     //数组
+    private String[] data;     //数组
 
     public ArrayStack(int n) {
         if (n<=0) n = 10;
-        data = new int[n];
+        data = new String[n];
         size = n;
         count = 0;
     }
@@ -21,7 +21,7 @@ public class ArrayStack {
     /**
      * 入栈
      */
-    public boolean push(int value) {
+    public boolean push(String value) {
         if (count==size) return false;
         data[count] = value;
         ++count;
@@ -31,16 +31,33 @@ public class ArrayStack {
     /**
      * 出栈
      */
-    public int pop() {
-        if (count == 0) return -1;
-        int tmp = data[count-1];
+    public String pop() {
+        if (count == 0) return "-1";
+        String tmp = data[count-1];
         --count;
+        data[count] = null;
         return tmp;
+    }
+
+    public void printAll() {
+        for (String datum : data) {
+            System.out.print(datum + " ");
+        }
+        System.out.println();
     }
 
 
     public static void main(String[] args) {
+        ArrayStack arrayStack = new ArrayStack(5);
+        arrayStack.push("1");
+        arrayStack.push("2");
+        arrayStack.push("3");
+        arrayStack.push("4");
 
+        arrayStack.printAll();
+
+        System.out.println(arrayStack.pop());
+        arrayStack.printAll();
     }
 
 }
